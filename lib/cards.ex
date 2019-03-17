45f89b9f3baf5dc84303403b1e1fc9f7860811b3
Eva <evangeline013@gmail.com>
2019-03-17 15:25:30 -0400

    Add function to create deck of cards, shuffle cards, and check if a card exists in a deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,20 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
-  @doc """
-  Hello world.
+  def create_deck do
+    suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
